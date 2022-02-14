595963a5c771ab24b6881cf1e681b9a74f15f739
hellenrincon <hellensoler31@gmail.com>
2022-02-14 17:01:50 -0500

    Hellen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeroComponent } from './components/primero/primero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeroComponent
   ],
   imports: [
     BrowserModule,
